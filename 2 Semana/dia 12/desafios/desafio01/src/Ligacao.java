6a8ac5552daa85a7bb1298e35fbe33f964f3c304
Allan8606 <allan-musico@hotmail.com>
2025-05-30 18:38:14 -0300

    Atualizando dia 12

--- 2 Semana/dia 12/desafios/desafio01/src/Ligacao.java
+++ 2 Semana/dia 12/desafios/desafio01/src/Ligacao.java
@@ -2,16 +2,19 @@ import java.util.List;
 
 public class Ligacao implements ReceberLigacao {
     List<String> numeros;
-
-
+    List<String> numerosJaAtendidos;
     String numeroTelefone;
 
-  // Todo: Método que vai receber as ligações e add os numeros de telefone na fila
+
+    public Ligacao(List<String> numeros, List<String> numerosJaAtendidos, String numeroTelefone) {
+        this.numeros = numeros;
+        this.numerosJaAtendidos = numerosJaAtendidos;
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    // Todo: Método que vai receber as ligações e add os numeros de telefone na fila
     @Override
     public void receberLigacao(List<String> numeros, String numeroTelefone){
-        this.numeros = numeros;
-        this.numeroTelefone = numeroTelefone;
-
         numeros.add(numeroTelefone);
         System.out.println("O número: " + numeroTelefone + " foi adicionado na fila para o atendimento");
         System.out.println("Fila de telefones para serem atendidos: " + numeros);
@@ -19,15 +22,19 @@ public class Ligacao implements ReceberLigacao {
     }
 
     //TODO: Método que vai tirar o primeiro numero que chegou da lista
-    public void retirandoNumero (List<String> numerosAtendidos){
-        if (numerosAtendidos.isEmpty()){
+    public void retirandoNumero (List<String> numerosAtendidos) {
+        if (numerosAtendidos.isEmpty()) {
             System.out.println("Parabéns todos seu atendimentos foram feitos");
+            System.out.println("Lista de números que já foram atendidos: " + numerosJaAtendidos);
             System.exit(0);
         }else {
             String numeroRetirado = numerosAtendidos.get(0);
             numerosAtendidos.remove(numeroRetirado);
-            System.out.println("O número: " + numeroRetirado + " foi atendido com sucesso");
+            numerosJaAtendidos.add(numeroRetirado);
+            System.out.println("O número: " + numeroRetirado + " foi atendido com sucesso.");
+            System.out.println("-----------------------");
             System.out.println("Lista atualizada de números para serem atendidos: " + numeros);
+            System.out.println("Lista de números que já foram atendidos: " + numerosJaAtendidos);
         }
 
 
--- 2 Semana/dia 12/desafios/desafio01/src/Main.java
+++ 2 Semana/dia 12/desafios/desafio01/src/Main.java
@@ -9,11 +9,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         List<String> listaDeNumeros = new ArrayList<>();
         List<String> numerosAtendidos = new ArrayList<>();
-        Ligacao ligacao = new Ligacao();
+        String numeroDeTelefone = "";
 
-        System.out.println("Deseja receber ligaça e adicionar ela na fila de atendimento? sim/não");
+        Ligacao ligacao = new Ligacao(listaDeNumeros, numerosAtendidos, numeroDeTelefone);
+
+        System.out.println("Deseja receber ligação e adicionar ela na fila de atendimento? Sim / Sair");
         String resposta = scanner.nextLine();
-        String numeroDeTelefone ;
+
 
         while (true){
 
@@ -22,16 +24,22 @@ public class Main {
                 System.out.println("Qual numero está ligando?");
                 numeroDeTelefone = scanner.nextLine();
                 ligacao.receberLigacao(listaDeNumeros, numeroDeTelefone);
-                System.out.println("Deseja atender outro número ou fazer um atendimento: Sim/Não/Atendimento");
+                System.out.println("Deseja atender outro número ou fazer um atendimento: Sim/Sair/Atendimento");
                 resposta = scanner.nextLine();
 
-            } else if (resposta.equalsIgnoreCase("sim") || resposta.equalsIgnoreCase("nao") || resposta.equalsIgnoreCase("não") || resposta.equalsIgnoreCase("atendimento")) {
+            } else if (!resposta.equalsIgnoreCase("sim") && !resposta.equalsIgnoreCase("sair") && !resposta.equalsIgnoreCase("atendimento")) {
                 System.out.println("Digite uma resposta valida: ");
                 System.out.println("------------");
-                System.out.println("Deseja receber ligação e adicionar ela na fila de atendimento ou fazer um atendimento? Sim/Não/Atendimento");
+                System.out.println("Deseja receber ligação e adicionar ela na fila de atendimento ou fazer um " +
+                        "atendimento? Sim / Sair/ Atendimento");
                 resposta = scanner.nextLine();
+
             } else if (resposta.equalsIgnoreCase("atendimento")) {
+
                 ligacao.retirandoNumero(listaDeNumeros);
+                System.out.println("Deseja receber ligação e adicionar ela na fila de atendimento ou fazer um " +
+                        "atendimento? Sim/ Sair/ Atendimento");
+                resposta = scanner.nextLine();
 
             } else {
                 System.out.println("Saindo...");
